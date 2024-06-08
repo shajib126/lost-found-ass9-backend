357eafb3bfcbaa8dbff7c1c0a70d6f877d882045
shajib126 <shajib126@gmail.com>
2024-06-08 11:07:22 +0600

    global error handled

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,7 +1,7 @@
 import { Prisma } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
-
+//global error handle
 const globalErrorHandler = (err:any,req:Request,res:Response,next:NextFunction)=>{
     let statusCode = httpStatus.INTERNAL_SERVER_ERROR;
     let success = false
